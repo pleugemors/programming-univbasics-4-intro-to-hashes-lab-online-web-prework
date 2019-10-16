ca07510339d9407205b9e5db85cbd82d4edcde3e
Paul Leugemors <pleugemors@yahoo.com>
2019-10-16 19:20:08 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -5,18 +5,22 @@ end
 
 def my_hash
   # return a valid hash with any key/value pair of your choice
+  my_creation = {:name => "Paul"} 
 end
 
 def pioneer
   # return a hash with a key of :name and a corresponding value of 'Grace Hopper'
+  new_hash = {:name => "Grace Hopper"}
 end
 
 def id_generator
   # return a hash with a key :id assigned to positive integer
+  id_hash = {:id => 1}
 end
 
 def my_hash_creator(key, value)
   # return a hash that includes the key and value parameters passed into this method
+  new_hash = {key => value}
 end
 
 def read_from_hash(hash, key)
